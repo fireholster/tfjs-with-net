e52040705a19a188b2ea118fc8f9dd313fdb9c75
fireholster <bikrant.sharma@gmail.com>
2021-05-03 13:29:40 +0530

    Add project files.

---
+++ WebApplication1/ClientApp/src/Services/TFService.ts
@@ -0,0 +1,15 @@
+﻿
+
+export interface ITFService {
+    checkConsole: any
+
+}
+
+class TFService implements ITFService {
+    
+    checkConsole(): any {
+        console.log("Inside TesortFlow Service");
+    }
+
+
+}
---
+++ WebApplication1/ClientApp/src/components/TensorFlow.js
@@ -0,0 +1,142 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var react_redux_1 = require("react-redux");
+var TFStore = require("../store/TF");
+var tf = require("@tensorflow/tfjs");
+var tfvis = require("@tensorflow/tfjs-vis");
+var TensorFlow = /** @class */ (function (_super) {
+    __extends(TensorFlow, _super);
+    function TensorFlow() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    // This method is called when the component is first added to the document
+    TensorFlow.prototype.componentDidMount = function () {
+    };
+    // This method is called when the route parameters change
+    TensorFlow.prototype.componentDidUpdate = function () {
+    };
+    TensorFlow.prototype.render = function () {
+        return (React.createElement(React.Fragment, null,
+            React.createElement("h1", null, "Inside TensorFlow component"),
+            this.runTensorFlowTest(),
+            this.run()));
+    };
+    TensorFlow.prototype.checkConsole = function () {
+        console.log("Inside TensorFlow Component");
+    };
+    TensorFlow.prototype.runTensorFlowTest = function () {
+        console.log("Testing");
+        // Define a model for linear regression.
+        var model = tf.sequential();
+        model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+        model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+        // Generate some synthetic data for training.
+        var xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+        var ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+        // Train the model using the data.
+        model.fit(xs, ys, { epochs: 10 }).then(function () {
+            // Use the model to do inference on a data point the model hasn't seen before:
+            model.predict(tf.tensor2d([5], [1, 1])).print();
+            // Open the browser devtools to see the output
+        });
+    };
+    TensorFlow.prototype.getCarsData = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var carsDataResponse, carsData, cleaned;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, fetch('https://storage.googleapis.com/tfjs-tutorials/carsData1.json')];
+                    case 1:
+                        carsDataResponse = _a.sent();
+                        return [4 /*yield*/, carsDataResponse.json()];
+                    case 2:
+                        carsData = _a.sent();
+                        cleaned = carsData.map(function (car) { return ({
+                            mpg: car.Miles_per_Gallon,
+                            horsepower: car.Horsepower,
+                        }); })
+                            .filter(function (car) { return (car.mpg != null && car.horsepower != null); });
+                        return [2 /*return*/, cleaned];
+                }
+            });
+        });
+    };
+    TensorFlow.prototype.run = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var data, values;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.getCarsData()];
+                    case 1:
+                        data = _a.sent();
+                        values = data.map(function (d) { return ({
+                            x: d.horsepower,
+                            y: d.mpg,
+                        }); });
+                        tfvis.render.scatterplot({ name: 'Horsepower v MPG' }, { values: values }, {
+                            xLabel: 'Horsepower',
+                            yLabel: 'MPG',
+                            height: 300
+                        });
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return TensorFlow;
+}(React.PureComponent));
+;
+exports.default = react_redux_1.connect(function (state) { return state.counter; }, TFStore.actionCreators)(TensorFlow);
+//# sourceMappingURL=TensorFlow.js.map
